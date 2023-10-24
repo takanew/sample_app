02e82640a2d963bff23ff326eae1f2036c0332de
takanew <145203231+takanew@users.noreply.github.com>
2023-10-24 06:54:13 +0000

    [Finish] 3-2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,17 @@
 class ListsController < ApplicationController
   def new
+    # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
+    @list = List.new  
+  end
+  
+  # 以下を追加
+  def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    # 4. トップ画面へリダイレクト
+    redirect_to '/top'
   end
 
   def index
@@ -10,4 +22,11 @@ class ListsController < ApplicationController
 
   def edit
   end
+  
+  private
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body)
+  end
+  
 end
