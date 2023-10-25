8b33d975500d448ba4905ecc1a0a897b47f14ead
takanew <145203231+takanew@users.noreply.github.com>
2023-10-25 23:04:00 +0000

    [Finish]3-7

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  post 'lists' => 'lists#create'
-  get 'lists' => 'lists#index'
-  # .../lists/1 や .../lists/3 に該当する
-  get 'lists/:id' => 'lists#show', as: 'list'
-  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
-  patch 'lists/:id' => 'lists#update', as: 'update_list'
-  get '/top' => 'homes#top'
+
+  get 'top' => 'homes#top'
+  resources :lists
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
