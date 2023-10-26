994f7a38a7282818261268311e17bcc4f03b079d
takanew <145203231+takanew@users.noreply.github.com>
2023-10-26 12:50:23 +0000

    [Finish]3-11update

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,7 +6,7 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
-      redirect_to list_path(list.id)
+      redirect_to list_path(@list.id)
     else
       render :new
     end
